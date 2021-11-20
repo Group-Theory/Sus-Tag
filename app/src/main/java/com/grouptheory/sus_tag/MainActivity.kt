334b2fbb579223c7c9f4ddb9693bc4990aa84a12
Jackson Bullard <jsbullar@uark.edu>
2021-11-20 01:30:18 -0600

    Added notification channel instantiation.

--- app/src/main/java/com/grouptheory/sus_tag/MainActivity.kt
+++ app/src/main/java/com/grouptheory/sus_tag/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.grouptheory.sus_tag
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +13,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.NotificationCompat
 import com.grouptheory.sus_tag.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +23,12 @@ class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		// Required to serve notifications
+		createNotificationChannel()
+
+		// TO-DO
+		// TEST: Send bogus notification on startup
 
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
@@ -54,5 +65,25 @@ class MainActivity : AppCompatActivity() {
 		val navController = findNavController(R.id.nav_host_fragment_content_main)
 		return navController.navigateUp(appBarConfiguration)
 				|| super.onSupportNavigateUp()
+	}
+
+	private fun createNotificationChannel() {
+		// Builds a notification channel used for tracker detection alerts
+		// Only on API 26+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+			val notificationId = getString(R.string.channel_id)
+			val name = getString(R.string.channel_name)
+			val desc = getString(R.string.channel_desc)
+			val priority = NotificationManager.IMPORTANCE_HIGH
+			val channel = NotificationChannel(notificationId, name, priority)
+					.apply { description = desc }
+
+			// After building notification, register it with the OS
+			val notificationManager: NotificationManager =
+				getSystemService(Context.NOTIFICATION_SERVICE)
+					as NotificationManager
+
+			notificationManager.createNotificationChannel(channel)
+		}
 	}
 }
